f1bb146d6d6a1b0c65ca4bc870b8e06c46a9d550
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-03 16:15:26 +0500

    -Users Uploading Routes Impl (#12)

---
+++ src/main/kotlin/com/example/plugins/Routes.kt
@@ -0,0 +1,41 @@
+package com.example.plugins
+
+import com.example.domain.model.Users
+import com.example.domain.reppository.UsersRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.users(
+    db: UsersRepository
+) {
+    post("v1/users") {
+        val parameters = call.receive<Parameters>()
+        val userName = parameters["username"] ?: return@post call.respondText(
+            text = "Username Missing",
+            status = HttpStatusCode.Unauthorized
+        )
+        val email = parameters["email"] ?: return@post call.respondText(
+            text = "Email MISSING",
+            status = HttpStatusCode.Unauthorized
+        )
+        val password = parameters["password"] ?: return@post call.respondText(
+            text = "Password Missing",
+            status = HttpStatusCode.Unauthorized
+        )
+        try {
+            val users = db.insert(userName,email, password)
+            users?.id.let {
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    "Uploaded to Server Successfully $users"
+                )
+            }
+        } catch (e: Exception) {
+            call.respond(status = HttpStatusCode.Unauthorized, "Error While Upload Data to Server ${e.message}")
+        }
+    }
+
+}
