debd5e2e0e69d5bc0e023c2eb67905b7f5949947
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-03 16:16:47 +0500

    -Users Database Factory Init (#14)

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,18 @@
 package com.example.plugins
 
+import com.example.data.local.DatabaseFactory
+import com.example.domain.reppository.UsersRepository
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    DatabaseFactory.init()
+    val db = UsersRepository()
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        users(db)
     }
 }
