a5132c9b0f48516dc40d27a66cfd8eca8c2d5ab1
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-03 15:22:27 +0500

    -Hikari Database Setup (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val postgres_version: String by project
+val hikaricp_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -40,4 +43,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
 }
---
+++ src/main/kotlin/com/example/data/local/DatabaseFactory.kt
@@ -0,0 +1,34 @@
+package com.example.data.local
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+    fun init() {
+        Database.connect(hikari())
+        transaction {
+
+        }
+    }
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = System.getenv("JDBC_DRIVER")
+        config.jdbcUrl = System.getenv("JDBC_DATABASE_URL")
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        return HikariDataSource(config)
+    }
+
+    suspend fun <T> dbQuery(block: () -> T): T =
+        withContext(Dispatchers.IO) {
+            transaction {
+                block()
+            }
+        }
+}
