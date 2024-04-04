738123d2a2b74473774e30cf2590da16992ce008
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-04 12:08:59 +0500

    -Categories Table Impl (#41)

---
+++ src/main/kotlin/com/example/data/local/table/category/CategoryTable.kt
@@ -0,0 +1,12 @@
+package com.example.data.local.table.category
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+
+object CategoryTable: Table("Categories") {
+    val id: Column<Long> = long("id").autoIncrement()
+    val name: Column<String> = varchar("name", length = 100).uniqueIndex()
+    val description : Column<String> = varchar("description", 151)
+    val isVisible: Column<Boolean> = bool("isVisible")
+    val imageUrl : Column<String> = varchar("imageUrl", 200)
+}
