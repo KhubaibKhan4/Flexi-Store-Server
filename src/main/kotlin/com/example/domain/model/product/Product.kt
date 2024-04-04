4a4d08b1498147904f24f8bfba806f7a5ef63dc4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-04 16:42:15 +0500

    -Product Model Impl (#68)

---
+++ src/main/kotlin/com/example/domain/model/product/Product.kt
@@ -0,0 +1,23 @@
+package com.example.domain.model.product
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Product(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val price: Long,
+    val categoryId: Long,
+    val imageUrl: String,
+    val created_at: String,
+    val updated_at: String,
+    val total_stack: Long,
+    val brand: String,
+    val weight: Double,
+    val dimensions: String,
+    val isAvailable: Boolean,
+    val discountPrice: Long,
+    val promotionDescription: String,
+    val averageRating: Double,
+)
