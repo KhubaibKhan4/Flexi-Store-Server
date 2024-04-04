beab5c1bc879aae20943f8713ef9f54ec33d76cc
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-04 20:39:22 +0500

    -Products DB Impl (#87)

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.example.plugins
 
 import com.example.data.local.table.db.DatabaseFactory
 import com.example.domain.reppository.category.CategoryRepository
+import com.example.domain.reppository.product.ProductRepository
 import com.example.domain.reppository.user.UsersRepository
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -11,11 +12,13 @@ fun Application.configureRouting() {
     DatabaseFactory.init()
     val db = UsersRepository()
     val categoriesDb = CategoryRepository()
+    val productsDb = ProductRepository()
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
         users(db)
         category(categoriesDb)
+        products(productsDb)
     }
 }
