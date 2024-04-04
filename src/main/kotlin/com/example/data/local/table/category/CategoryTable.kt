8c9702a0fedd17075b3ec9a77d8b365f239cebf0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-04 20:47:01 +0500

    -Categories & Products Enhancement (#89)

--- src/main/kotlin/com/example/data/local/table/category/CategoryTable.kt
+++ src/main/kotlin/com/example/data/local/table/category/CategoryTable.kt
@@ -6,7 +6,9 @@ import org.jetbrains.exposed.sql.Table
 object CategoryTable: Table("Categories") {
     val id: Column<Long> = long("id").autoIncrement()
     val name: Column<String> = varchar("name", length = 100).uniqueIndex()
-    val description : Column<String> = varchar("description", 151)
+    val description : Column<String> = varchar("description", length = 151)
     val isVisible: Column<Boolean> = bool("isVisible")
-    val imageUrl : Column<String> = varchar("imageUrl", 200)
+    val imageUrl : Column<String> = varchar("imageUrl", length = 200)
+
+    override val primaryKey: PrimaryKey?= PrimaryKey(id)
 }
--- src/main/kotlin/com/example/data/local/table/product/ProductTable.kt
+++ src/main/kotlin/com/example/data/local/table/product/ProductTable.kt
@@ -11,7 +11,7 @@ object ProductTable:Table("Product") {
     val price: Column<Long> = long("price")
     val categoryId: Column<Long> = long("category_id").references(CategoryTable.id)
     val categoryTitle: Column<String> = varchar("category_title", length = 51).references(CategoryTable.name)
-    val imageUrl : Column<String> = varchar("imageUrl", length = 51)
+    val imageUrl: Column<String> = varchar("imageUrl", length = 51)
     val created_at: Column<String> = varchar("created_at", length = 51)
     val updated_at: Column<String> = varchar("updated_at", length = 51)
     val total_stack: Column<Long> = long("total_stack")
@@ -21,5 +21,7 @@ object ProductTable:Table("Product") {
     val isAvailable: Column<Boolean> = bool("isAvailable")
     val discountPrice: Column<Long> = long("discountPrice")
     val promotionDescription: Column<String> = varchar("promotionDescription", length = 151)
-    val averageRating: Column<Double> = double("promotionDescription")
+    val averageRating: Column<Double> = double("averageRating")
+
+    override val primaryKey: PrimaryKey? = PrimaryKey(id)
 }
