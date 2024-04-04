f0e4c22ad66c0fd97a5b73eb5b6d375155910d25
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-04 11:28:40 +0500

    -Users Role Enhancement (#32)

--- src/main/kotlin/com/example/data/local/table/user/UserTable.kt
+++ src/main/kotlin/com/example/data/local/table/user/UserTable.kt
@@ -8,6 +8,11 @@ object UserTable : Table("Users") {
     val username: Column<String> = varchar("username", length = 51)
     val email: Column<String> = varchar("email", length = 51)
     val password: Column<String> = varchar("password", length = 51)
+    val address: Column<String> = varchar("address", length = 150)
+    val city: Column<String> = varchar("city", length = 75)
+    val country: Column<String> = varchar("country", length = 100)
+    val phoneNumber: Column<String> = varchar("phoneNumber", length = 130)
+    val userRole: Column<String> = varchar("userRole", length = 51)
 
     override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
