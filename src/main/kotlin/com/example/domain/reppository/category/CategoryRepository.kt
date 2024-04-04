c0cf0a8dbc87d74726c07a02424017c26ab22d92
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-04 12:32:21 +0500

    -Get All Categories Impl (#48)

--- src/main/kotlin/com/example/domain/reppository/category/CategoryRepository.kt
+++ src/main/kotlin/com/example/domain/reppository/category/CategoryRepository.kt
@@ -7,24 +7,30 @@ import com.example.data.repository.category.CategoryDao
 import com.example.domain.model.category.Category
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.statements.InsertStatement
 
-class CategoryRepository: CategoryDao {
+class CategoryRepository : CategoryDao {
     override suspend fun insert(name: String, description: String, isVisible: Boolean, imageUrl: String): Category? {
         var statement: InsertStatement<Number>? = null
         DatabaseFactory.dbQuery {
-            statement = CategoryTable.insert {category ->
+            statement = CategoryTable.insert { category ->
                 category[CategoryTable.name] = name
                 category[CategoryTable.description] = description
                 category[CategoryTable.isVisible] = isVisible
-                category[CategoryTable.imageUrl]= imageUrl
+                category[CategoryTable.imageUrl] = imageUrl
             }
         }
         return rowToResult(statement?.resultedValues?.get(0)!!)
     }
 
     override suspend fun getAllCategories(): List<Category>? {
-        TODO("Not yet implemented")
+        return DatabaseFactory.dbQuery {
+            CategoryTable.selectAll()
+                .mapNotNull {
+                    rowToResult(it)
+                }
+        }
     }
 
     override suspend fun getCategoryById(id: Long): Category? {
@@ -44,12 +50,13 @@ class CategoryRepository: CategoryDao {
     ): Int? {
         TODO("Not yet implemented")
     }
-    private fun rowToResult(row: ResultRow): Category?{
-        if (row== null){
+
+    private fun rowToResult(row: ResultRow): Category? {
+        if (row == null) {
             return null
-        }else{
+        } else {
             return Category(
-               id = row[CategoryTable.id],
+                id = row[CategoryTable.id],
                 name = row[CategoryTable.name],
                 description = row[CategoryTable.description],
                 isVisible = row[CategoryTable.isVisible],
