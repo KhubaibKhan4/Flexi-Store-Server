cf03129c2bf902033aa3f82a484f9d9003de5215
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-24 11:16:36 +0500

    -Cart Repository Impl (#146)

---
+++ src/main/kotlin/com/example/domain/repository/cart/CartRepository.kt
@@ -0,0 +1,26 @@
+package com.example.domain.repository.cart
+
+import com.example.data.repository.cart.CartDao
+import com.example.domain.model.cart.CartItem
+
+class CartRepository: CartDao {
+    override suspend fun insert(productId: Long, quantity: Int, product: Long): CartItem? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllCart(): List<CartItem>? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCartById(id: Long): CartItem? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCartById(id: Long): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(productId: Long, quantity: Int, product: Long) {
+        TODO("Not yet implemented")
+    }
+}
