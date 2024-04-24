ad46844734d7233eb25a7c820c700bdbfd4fc751
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-24 11:44:10 +0500

    -Cart Repository Enhancement (#150)

--- src/main/kotlin/com/example/domain/repository/cart/CartRepository.kt
+++ src/main/kotlin/com/example/domain/repository/cart/CartRepository.kt
@@ -4,7 +4,7 @@ import com.example.data.repository.cart.CartDao
 import com.example.domain.model.cart.CartItem
 
 class CartRepository : CartDao {
-    override suspend fun insert(productId: Long, quantity: Int, product: Long): CartItem? {
+    override suspend fun insert(productId: Long, quantity: Int, userId: Long): CartItem? {
         TODO("Not yet implemented")
     }
 
@@ -12,15 +12,16 @@ class CartRepository : CartDao {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getCartById(id: Long): CartItem? {
+    override suspend fun getCartByUserId(id: Long): CartItem? {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteCartById(id: Long): Int? {
+    override suspend fun deleteCartByUserId(id: Long): Int? {
         TODO("Not yet implemented")
     }
 
-    override suspend fun update(productId: Long, quantity: Int, product: Long) {
+    override suspend fun update(productId: Long, quantity: Int, userId: Long) {
         TODO("Not yet implemented")
     }
+
 }
