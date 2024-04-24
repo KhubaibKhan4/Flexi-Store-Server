07dc46c84799765f1382010dcbdba0e7d357be8d
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-24 11:38:07 +0500

    -Cart Items Model Impl (#147)

--- src/main/kotlin/com/example/domain/model/cart/Cart.kt
+++ src/main/kotlin/com/example/domain/model/cart/Cart.kt
@@ -1,11 +1,10 @@
 package com.example.domain.model.cart
 
-import com.example.domain.model.product.Product
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CartItem(
     val productId: Long,
     val quantity: Int,
-    val product: Product
+    val userId: Long
 )
--- src/main/kotlin/com/example/domain/repository/cart/CartRepository.kt
+++ src/main/kotlin/com/example/domain/repository/cart/CartRepository.kt
@@ -3,7 +3,7 @@ package com.example.domain.repository.cart
 import com.example.data.repository.cart.CartDao
 import com.example.domain.model.cart.CartItem
 
-class CartRepository: CartDao {
+class CartRepository : CartDao {
     override suspend fun insert(productId: Long, quantity: Int, product: Long): CartItem? {
         TODO("Not yet implemented")
     }
