c6e0100ba20d656ade769d3210027a5c9af15d91
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-24 11:41:02 +0500

    -Table Enhancement (#148)

--- src/main/kotlin/com/example/data/local/table/cart/CartTable.kt
+++ src/main/kotlin/com/example/data/local/table/cart/CartTable.kt
@@ -1,11 +1,11 @@
 package com.example.data.local.table.cart
 
-import com.example.data.local.table.product.ProductTable
+import com.example.data.local.table.user.UserTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object CartTable: Table("Cart") {
     val productId: Column<Long> = long("productId")
     val quality: Column<Int> = integer("quantity")
-    val product: Column<Long> = long("product").references(ProductTable.id)
+    val userId: Column<Long> = long("userId").references(UserTable.id)
 }
