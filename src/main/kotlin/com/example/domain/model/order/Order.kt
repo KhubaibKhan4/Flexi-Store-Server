db88c28c999621804f073879ec9bc505a02e96f0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-28 12:07:36 +0500

    -Order Model Impl (#170)

---
+++ src/main/kotlin/com/example/domain/model/order/Order.kt
@@ -0,0 +1,15 @@
+package com.example.domain.model.order
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Order(
+    val id: Long,
+    val userId : Long,
+    val productIds: String,
+    val totalQuantity: Int,
+    val totalPrice: Double,
+    val orderProgress: String,
+    val paymentType: String,
+    val trackingId: String,
+)
